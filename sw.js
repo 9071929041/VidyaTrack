6f1c2caa7ea7a00055f0e838f072aff6849c315f
Prashanth <prashanthchoodappanavar@gmail.com>
2026-09-11 19:42:25 +0530

    Bust service worker cache and force reload

--- sw.js
+++ sw.js
@@ -1,6 +1,15 @@
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((k) => caches.delete(k)));
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request));
+  e.respondWith(fetch(e.request, { cache: 'no-store' }));
 });
